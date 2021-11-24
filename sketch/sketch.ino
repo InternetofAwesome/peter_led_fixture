a4070b6423a1ad762a2c5d270f37964e8862a457
Sam Anderson <sam@juul.com>
2021-11-23 23:35:04 -0800

    Change from pot to rotary encoder

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -29,11 +29,16 @@
 
 #include "Adafruit_NeoPixel.h"
 #include <EEPROM.h>
+#include "RotaryEncoder.h"
 
-#define HUE_PIN 2
-#define SAT_PIN 3
+#define HUE_PIN 9 //B1
+#define SAT_PIN 10 //B2
+#define BUT_PIN 11 //B3
+//rotary encoder pins
+#define ENC0 A0 //C8
+#define ENC1 A1 //C9
 #define NUM_LEDS 8*8
-#define HUE_ADJ_TIMEOUT 2000 //amount of time you have after hitting the button to adjust the hue, milliseconds
+#define ADJ_TIMEOUT 60000 //amount of time you have after hitting the button to adjust the hue, milliseconds
 #define ROLL_AVG_N 50
 #define VALUE_INC 4 //amount to increment/decrement value while moving toward target_value
 #define EFFECT_SPEED 2 //number of LED updates between incrementing the "effect" along the strands
@@ -51,6 +56,10 @@ uint8_t spins[] = {4, 5, 6, 7, 8};
 //uint8_t snum[] = {88, 121, 119, 116, 71};
 uint8_t snum[] = {71, 116, 119, 121, 88};
 
+//Rotary encoder null object
+RotaryEncoder *encoder = nullptr;
+
+
 //initialize our pixel strand objects
 Adafruit_NeoPixel strand[] = {
   Adafruit_NeoPixel(snum[0], spins[0], NEO_GRB + NEO_KHZ800),
@@ -60,16 +69,31 @@ Adafruit_NeoPixel strand[] = {
   Adafruit_NeoPixel(snum[4], spins[4], NEO_GRB + NEO_KHZ800),
 };
 
+typedef enum {
+  MODE_NORM,
+  MODE_HUE,
+  MODE_SAT
+} mode_adj_t;
+
+//flag for enabling "easter egg" mode
+bool easter_egg = 0;
+
 //global value for the brightness being displayed
 uint8_t value = 128;
-//global value for the brightness we WANT, so we can smoothly transition to it
-uint8_t target_value;
+uint8_t brightness = 8; //value to convert to Value (ie, HSV value)
+//adjustment (hue and sat) timeout
+uint32_t adj_timeout = 0;
+//track the current LED strand for hue/sat adjustment
+uint8_t strand_index=0;
+//track what mode we are in (normal, hue adj, sat adj)  
+mode_adj_t mode = MODE_NORM;
 
 //hue and sat values for each strand
 uint16_t hue[] = {0, 0, 0, 0, 0};
 uint8_t sat[] = {0, 0, 0, 0, 0};
-
-uint32_t value_sum; //used for running average of value
+volatile uint8_t hue_press;
+volatile uint8_t sat_press;
+volatile uint8_t but_press;
 
 void load_eeprom()
 {
@@ -97,10 +121,65 @@ void save_eeprom()
   }
 }
 
+
+// The Interrupt Service Routine for Pin Change Interrupt 1
+// This routine will only be called on any signal change on A2 and A3.
+  ISR(PCINT1_vect) {
+    encoder->tick(); // just call tick() to check the state.
+  }
+
+
+// The Interrupt Service Routine for Pin Change Interrupt 1
+// This routine will only be called on any signal change on A2 and A3.
+  ISR(PCINT0_vect) {
+    static unsigned long last = 0;
+    if(micros() - last < 10000)
+      return;
+    last = micros();
+    hue_press |= digitalRead(HUE_PIN);
+    sat_press |= digitalRead(SAT_PIN);
+    but_press |= !digitalRead(BUT_PIN);
+    PCIFR &= ~PCIF0;
+//    PCMSK0 &= ~(1<<PCINT1 | 1<<PCINT2 | 1<<PCINT3) ;
+  }
+
 //convenience function to give me a usable color value from HSB
 uint32_t get_color(uint16_t h, uint8_t s, uint8_t v)
 {
   return strand[0].gamma32(strand[0].ColorHSV(h, s, v));
+}
+
+uint16_t range_checked_update(uint16_t val, uint16_t max, bool wrap)
+{
+  
+  int16_t enc_pos = encoder->getPosition();
+  if( enc_pos == 0)
+    return val;
+  uint16_t speed = encoder->getRPM();
+  encoder->setPosition(0);
+  if(speed > 3 && speed < 10)
+    enc_pos *= (max / 128) ;
+  else if(speed >=10 && speed < 100)
+    enc_pos *= (max / 64);
+  else if(speed >= 100)
+    enc_pos *= (max / 32);
+  if(wrap)
+    val += enc_pos & max;
+  else
+    if(((int32_t)val + (int32_t)enc_pos) > (int32_t)max)
+      val = max;
+    else if(((int32_t)val + (int32_t)enc_pos) < 0)
+      val = 0;
+    else
+      val += enc_pos; 
+  
+  Serial.print("value ");
+  Serial.print(val);
+  Serial.print(", speed ");
+  Serial.print(speed);
+  Serial.print(", offset ");
+  Serial.println(enc_pos);
+  return val;
 }
 
 //this does a lot of the heavy lifting of setting colors and running animations
@@ -109,88 +188,100 @@ void update()
   static uint8_t effect_location[] = {0,0,0,0,0}; //keeps track of where the effect starts
   static uint8_t effect_counter=0; //counts frames between an effect change
   static uint32_t next_run[] = {0,0,0,0,0}; //time index of when we should start the next effect
-  //adjust the brightness value
-  if(target_value != value) //only adjust value if they are different
+  static uint8_t easter_location = 0;
+  //adjust the brightness value (do something here)
+
+  //do easter egg mode
+  if(easter_egg)
   {
-  	//if target value and value are only off by less than the increment amount, just set them equal
-    if(abs(target_value - value) < VALUE_INC) 
-      value = target_value; //then just set them equal
-    else if(target_value - value > 0) //find out if we need to increment or decrement value
-      value += VALUE_INC;
-    else
-      value -= VALUE_INC;
-  } //note brightness is not actually applied in this block, just calculated
-  
-  //go through each strand, and apply effect, if applicable, and update value/hue/sat
-  for(int i=0; i<sizeof(snum); i++)
-  { 
-  	//fill the entire strand memory buffer with the right HSV
-    strand[i].fill(get_color(hue[i], sat[i], value),  0, snum[i]); 
-    //if were at, or later than the right time to apply the effect, and the effect loaction is out
-    //of bounds, indicating that it has not been set
-    if(next_run[i] < millis() && effect_location[i] == 0xff)
+    
+    for(int i=0; i<sizeof(snum); i++)
     {
-      //then set it to zero, which is where it will start
-      effect_location[i] = 0;
-    }
-    //if the effect location is valid, then apply the effect
-    if(effect_location[i] != 255)
-    {
-      //iterate over all elements of the "effect" values
       for(int j=0; j<sizeof(effect); j++)
       {
-      	//calculate the value for the pixel affected by the effect
-        uint32_t eff_bright = value + effect[j]/EFFECT_BRIGHTNESS*value/255;
-
-        //if the calculated brightness exceeds max, then set it to max
-        if(eff_bright & 0xffffff00)
-          eff_bright = 0xff; //255 (0xff) is max brightness
-        //set the appropriate pixel value to the brightness it should be for the location of the effect
-        strand[i].setPixelColor(effect_location[i]-sizeof(effect)+j, get_color(hue[i], sat[i], (uint8_t)eff_bright)); //this will mess up at the upper end of brightness
+        strand[i].setPixelColor(easter_location-sizeof(effect)+j, get_color((hue[i]+effect[j]*256) & 0xefff, sat[i], value)); 
       }
+      //pump out new values to all the pixels
+      strand[i].show();
     }
-    //check to see if we should increment the location of the effect (we only want to do this every
-    // EFFECT_SPEED frames)
-    if(!(effect_counter % EFFECT_SPEED))
+    //increment the index of where the easter egg "effect" starts
+    easter_location++;
+    if(easter_location > 121)
     {
-      //increment the index of where the "effect" starts
-      effect_location[i]++;
+      easter_egg = 0;
+      easter_location = 0;
     }
-
-    //check to see if the effect location is out of bounds of the LED strand. If so, set it to 255 
-    //(invalid value)
-    if((snum[i] + sizeof(effect)) < effect_location[i])
-    {
-      //again, 255 represents an invalid location, and means an effect is not running
-      effect_location[i] = 255;
-      //calculate a random time to start the next effect.
-      next_run[i] = millis()+random(EFFECT_TIME_MIN, EFFECT_TIME_MAX);
+    delay(15);
+  }
+  else
+  {
+    //go through each strand, and apply effect, if applicable, and update value/hue/sat
+    for(int i=0; i<sizeof(snum); i++)
+    { 
+    	//fill the entire strand memory buffer with the right HSV
+      strand[i].fill(get_color(hue[i], sat[i], value),  0, snum[i]); 
+      //if were at, or later than the right time to apply the effect, and the effect loaction is out
+      //of bounds, indicating that it has not been set
+      if(next_run[i] < millis() && effect_location[i] == 0xff)
+      {
+        //then set it to zero, which is where it will start
+        effect_location[i] = 0;
+      }
+      //if the effect location is valid, then apply the effect
+      if(effect_location[i] != 255)
+      {
+        //iterate over all elements of the "effect" values
+        for(int j=0; j<sizeof(effect); j++)
+        {
+        	//calculate the value for the pixel affected by the effect
+          uint32_t eff_bright = value + effect[j]/EFFECT_BRIGHTNESS*value/255;
+  
+          //if the calculated brightness exceeds max, then set it to max
+          if(eff_bright & 0xffffff00)
+            eff_bright = 0xff; //255 (0xff) is max brightness
+          //set the appropriate pixel value to the brightness it should be for the location of the effect
+          strand[i].setPixelColor(effect_location[i]-sizeof(effect)+j, get_color(hue[i], sat[i], (uint8_t)eff_bright)); //this will mess up at the upper end of brightness
+  
+        }
+      }
+      //check to see if we should increment the location of the effect (we only want to do this every
+      // EFFECT_SPEED frames)
+      if(!(effect_counter % EFFECT_SPEED))
+      {
+        //increment the index of where the "effect" starts
+        effect_location[i]++;
+      }
+  
+      //check to see if the effect location is out of bounds of the LED strand. If so, set it to 255 
+      //(invalid value)
+      if((snum[i] + sizeof(effect)) < effect_location[i])
+      {
+        //again, 255 represents an invalid location, and means an effect is not running
+        effect_location[i] = 255;
+        //calculate a random time to start the next effect.
+        next_run[i] = millis()+random(EFFECT_TIME_MIN, EFFECT_TIME_MAX);
+      }
+      //pump out new values to all the pixels
+      strand[i].show();
     }
-    //pump out new values to all the pixels
-    strand[i].show();
   }
   //increment our frame counter
   effect_counter++;
 }
 
-int read_pot()
-{
-  //read the pot. bitmask just for extra safety to make sure number is in expected range
-  return analogRead(sensorPin) & 0b1111111111;
-}
-
 //setup our pins and whatnot. This runs once
 void setup() {
+    Serial.begin(115200);
+  while (!Serial)
+    ;
+   Serial.println("Setting up stuff");
   //set GRPIO direction and pullup
   pinMode(HUE_PIN, INPUT_PULLUP);
   pinMode(SAT_PIN, INPUT_PULLUP);
+  pinMode(BUT_PIN, INPUT_PULLUP);
   //load saved values from EEPROM
   load_eeprom();
 
-  //get an initial value for the brightness
-  value = read_pot()>>2;
-  //set the rolling average total
-  value_sum = value*ROLL_AVG_N;
   //set each strand to the appropriate color
   for(int i=0; i<sizeof(snum); i++)
   {
@@ -199,127 +290,103 @@ void setup() {
     //clear it
     strand[i].clear();
     //set pixel values
-    strand[i].fill(strand[i].ColorHSV(hue[i], 255, value), 0, snum[i]); 
+    strand[i].fill(strand[i].ColorHSV(hue[i], sat[i], value), 0, snum[i]); 
     //pump out the values to LEDs
     strand[i].show();
   }
+  // use TWO03 mode when PIN_IN1, PIN_IN2 signals are both LOW or HIGH in latch position.
+  encoder = new RotaryEncoder(ENC0, ENC1, RotaryEncoder::LatchMode::FOUR0);
+//  attachInterrupt(digitalPinToInterrupt(ENC0), checkPosition, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(ENC1), checkPosition, CHANGE);
+  // Setup flags to activate the ISR PCINT1.
+// You may have to modify the next 2 lines if using other pins than A2 and A3
+
+  pinMode(ENC0, INPUT_PULLUP);
+  pinMode(ENC1, INPUT_PULLUP);
+
+  //Port C interrupt
+  PCICR = 1<<PCIE1 | 1<<PCIE0; // set int on port C and B
+  PCIFR = 0x00; //clear interrupt flags 
+  PCMSK1 = 1<<PCINT8 | 1<<PCINT9;
+  PCMSK0 = 1<<PCINT1 | 1<<PCINT2 | 1<<PCINT3 ;
+  
+//  EICRA = 0x03;
+//  attachInterrupt(digitalPinToInterrupt(ENC0), checkPosition, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(ENC1), checkPosition, CHANGE);
+  Serial.println("Stuff setup complete");
+  Serial.println(A1);
+
+  
+
+}
+
+void reset_timeout()
+{
+  adj_timeout = millis() + ADJ_TIMEOUT;
+}
+
+void enter_mode(mode_adj_t new_mode)
+{
+  Serial.print("mode: ");
+  Serial.println(new_mode);
+      if(new_mode == MODE_NORM)
+      {
+        save_eeprom();
+        //make sure our timeout is zeroed out
+        adj_timeout = 0;
+      }
+      else
+      {
+        if(new_mode == mode)
+          strand_index = (strand_index + 1) % sizeof(spins);
+        Serial.print("strand ");
+        Serial.println(strand_index);
+        //blank the currently controlled strand for a bit
+        strand[strand_index].fill(strand[strand_index].Color(0,0,0), 0, snum[strand_index]);
+        strand[strand_index].show();  
+        delay(100);
+        reset_timeout();
+      }
+      mode = new_mode;
 }
 
 //this runs over and over... Forever.
 void loop() {  
-  uint16_t pot;
-  //hue adjustment timeout
-  static uint32_t hue_adj_end = 0;
-  //saturation adjustment timeout
-  static uint32_t sat_adj_end = 0;
-  //previous pot position, used for timeout expiration
-  static uint16_t last_pot;
-  //track the current LED strand for hue/sat adjustment
-  static uint8_t strand_index=0;
-  //track offset of pot for relative hue adjustment
-  static uint16_t pot_hue_offset;
+  if(hue_press)
+  {
+    enter_mode(MODE_HUE);
+    hue_press = 0;
+    Serial.println("Hue press");
+  }
+  if(sat_press)
+  {
+    enter_mode(MODE_SAT);
+    sat_press = 0;
+    Serial.println("Sat press");
+  }
+  if(but_press)
+  {
+    if(mode == MODE_NORM)
+      easter_egg =1;
+    enter_mode(MODE_NORM);
+    but_press = 0;
+    Serial.println("But press");
+  }
 
-  //values to track button press values
-  uint8_t hue_button;
-  uint8_t sat_button;
-  //read button press values
-  hue_button = !digitalRead(HUE_PIN);
-  sat_button = !digitalRead(SAT_PIN);
-  
-  pot = read_pot(); //read the pot value *once* at the top of the loop
-  value_sum -= value_sum/ROLL_AVG_N; //subtract the average value from the rolling sum of the pot value
-  value_sum += pot>>2 & 0xff; //add the actual pot value to the rolling sum. ADC is 10 bits, so we shift right 2 to make the right range for the LED brightness of 0-255
-  
-  //if the hue button is down, OR our hue adjustment timeout has not elapsed
-  if(hue_button == 1 || hue_adj_end > millis()) 
+  //give us more time if we're in an adjustment mode, and there is encoder movement.
+  if(mode != MODE_NORM)
   {
-    //set a brightness target of a fixed (arbitrary) value 
-    target_value = 64;
-    //if our timeout has NOT elapsed, and the hue button has been pressed
-    if(hue_adj_end > millis() && hue_button == 1)
-    {
-      //increment the index of the strand we are adjusting
-      strand_index = (strand_index + 1) % sizeof(snum);
-      //set that strand to all black for a short amount of time, so we can identify it.
-      strand[strand_index].fill(strand[strand_index].Color(0,0,0), 0, snum[strand_index]);
-      strand[strand_index].show();  
-      delay(100);
-      
-      //reset our timeout to some time in the future
-      hue_adj_end = millis() + HUE_ADJ_TIMEOUT;
-    }
-    //if we do not have a timeout set
-    if(hue_adj_end == 0)
-    {
-      /* save our current pot location, so we can subtract it out. This will allow us to adjust the
-      hue RELATIVE to the current pot position, instead of just using the ABSOLUTE value of the pot
-      reading, which would instantly change the color when we changed to adjust that strand.
-      Also, the Hue controls are 16-bits, hence the shift left */
-      pot_hue_offset = pot << 6;
-      //blank the currently controlled strand for a bit
-      strand[strand_index].fill(strand[strand_index].Color(0,0,0), 0, snum[strand_index]);
-      strand[strand_index].show();  
-      delay(100);
-      //reset our tiemout
-      hue_adj_end = millis() + HUE_ADJ_TIMEOUT;
-    }
-    //this was an attempt to add some noise immunity. If there's too much noise, the adjustment 
-    //mode will never time out
-    if ( abs(pot - last_pot) > 20)
-    {
-       hue_adj_end = millis() + HUE_ADJ_TIMEOUT;
-       last_pot = pot;
-    }
-    //use our pot offset to get a new hue value, using the pot value
-    hue[strand_index] = hue[strand_index] + ((pot << 6) - pot_hue_offset);
-    //reset the pot offset (we always want to subtract it out)
-    pot_hue_offset = pot << 6;
+    if(adj_timeout < millis())
+      enter_mode(MODE_NORM);
+    else if(encoder->getPosition())
+      reset_timeout();
   }
-  //this block is almost exactly the same as the above if-block. See comments above
-  else if(hue_button == 1 || sat_adj_end > millis())
-  {
-    target_value = 64;
-    if(sat_adj_end > millis() && hue_button == 1)
-    {
-      strand_index = (strand_index + 1) % sizeof(snum);
-      strand[strand_index].fill(strand[strand_index].Color(0,0,0), 0, snum[strand_index]);
-      strand[strand_index].show();  
-      delay(100);
-      sat_adj_end = millis() + HUE_ADJ_TIMEOUT;
-    }
-    if(sat_adj_end == 0)
-    {
-      strand[strand_index].fill(strand[strand_index].Color(0,0,0), 0, snum[strand_index]);
-      strand[strand_index].show();  
-      delay(100);
-      sat_adj_end = millis() + HUE_ADJ_TIMEOUT;
-    }
-    if ( abs(pot - last_pot) > 20)
-    {
-       sat_adj_end = millis() + HUE_ADJ_TIMEOUT;
-       last_pot = pot;
-    }
-    //update the 8-bit saturation target
-    sat[strand_index] = pot >> 2; 
-  }
-  else
-  {
-  	//adjust the target value. See the update function for why that is important.
-    target_value = value_sum/ROLL_AVG_N;
-  }
-  
-  //if our hue adjustment timeout has elapsed, and hue adjustment timeout is non-zero (non-zero 
-  // means there is not one set). Same logic applies for the saturation timeout
-  if((hue_adj_end < millis() && hue_adj_end != 0) ||  (sat_adj_end < millis() && sat_adj_end != 0))
-  { 
-  	//save the hue and saturation values to EEPROM
-    save_eeprom();
-    //set target value - This may be redundant to the above setting operation.
-    target_value = value_sum/ROLL_AVG_N;
 
-    //make sure our timeouts are zeroed out
-    hue_adj_end = 0;
-    sat_adj_end = 0;
+  switch(mode)
+  {
+    case MODE_NORM: value = range_checked_update(value, 255, 0); break;
+    case MODE_HUE: hue[strand_index] = range_checked_update(hue[strand_index], 65535, 1); break;
+    case MODE_SAT: sat[strand_index] = range_checked_update(sat[strand_index], 255, 0); break;
   }
   //update the pixels
   update();
